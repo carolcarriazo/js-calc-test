c8405f49bf3951c24e6dac5eb77fe96dfcbbb30d
carolcarriazo <carolcarriazo@gmail.com>
2018-11-06 15:40:05 -0500

    adding logic

---
+++ calculator.js
@@ -0,0 +1,9 @@
+const calculator = document.querySelector('.calculator')
+const keys = document.querySelector('calculator_keys')
+
+keys.addEventListener('click', event=> {
+  if event.target.matches('button')) {
+    //dosomething
+  }
+  }
+})
