0b42526e3b99fbb08eef920a94bfc21b4ceb68e5
carolcarriazo <carolcarriazo@gmail.com>
2018-11-07 13:50:56 -0500

    adding logic

--- calculator.js
+++ calculator.js
@@ -7,3 +7,31 @@ keys.addEventListener('click', event=> {
   }
   }
 })
+
+const key = event.target
+const action = key.dataset.action
+
+if (!action) {
+  console.log('number key!')
+}
+
+if (
+  action === 'add' ||
+  action === 'subtract' ||
+  action === 'multiply' ||
+  action === 'divide'
+) {
+  console.log('operator key!')
+}
+
+if (action === 'decimal') {
+  console.log('decimal key!')
+}
+
+if (action === 'clear') {
+  console.log('clear key!')
+}
+
+if (action === 'calculate') {
+  console.log('equal key!')
+}
