4343cd678a398e3f68b760b60cfed4fe4e940a7e
carolcarriazo <carolcarriazo@gmail.com>
2018-11-30 16:20:47 -0500

    divide by 0 fix

---
+++ geturl.js
@@ -0,0 +1,6 @@
+/* const urlbutton = document.querySelector('.urlbutton')
+urlbutton.addEventListener('click', e => {
+  if (e.target.matches('.urlbutton')) {
+    console.log('clicked button!')
+  }
+}) */
